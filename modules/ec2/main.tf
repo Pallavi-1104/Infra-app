ba72574c046de650869d4f2a4ed7376937ced4eb
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-15 12:20:09 +0530

    qwertyui

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,17 +1,19 @@
 # modules/ec2/main.tf
 
+resource "random_id" "sg_id" {
+  byte_length = 8
+}
+
 resource "aws_security_group" "ecs_instance_sg" {
-  name        = "ecs-instance-sg"
+  name        = "ecs-instance-sg-${random_id.sg_id.hex}"  # Unique name
   description = "Security group for ECS EC2 instances"
   vpc_id      = var.vpc_id
-
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -20,14 +22,19 @@ resource "aws_security_group" "ecs_instance_sg" {
   }
 }
 
+
+resource "random_id" "role_id" {
+  byte_length = 8
+}
+
 resource "aws_iam_role" "ecs_instance_role" {
-  name = "ecs-instance-role"
+  name = "ecs-instance-role-${random_id.role_id.hex}"  # Unique role name
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
+        Effect    = "Allow"
         Principal = {
           Service = "ec2.amazonaws.com"
         }
@@ -37,22 +44,24 @@ resource "aws_iam_role" "ecs_instance_role" {
   })
 }
 
+
 resource "aws_iam_instance_profile" "ecs_instance_profile" {
   name = "ecs-instance-profile"
   role = aws_iam_role.ecs_instance_role.name
 }
 
+data "aws_ssm_parameter" "ecs_ami" {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+}
+
 resource "aws_launch_configuration" "ecs_launch_configuration" {
-  name_prefix          = "ecs-launch-config"
-  image_id             = "ami-00a929b66ed6e0de6" # Example Amazon Linux 2 ECS-optimized AMI
+  image_id             = data.aws_ssm_parameter.ecs_ami.value  # Get the latest ECS AMI from SSM
   instance_type        = "t2.micro"
   security_groups      = [aws_security_group.ecs_instance_sg.id]
   iam_instance_profile = aws_iam_instance_profile.ecs_instance_profile.name
   associate_public_ip_address = true
-  lifecycle {
-    create_before_destroy = true
-  }
 }
+
 
 resource "aws_autoscaling_group" "ecs_asg" {
   desired_capacity     = 1
