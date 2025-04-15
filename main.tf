336a62b2c636cc5ff1d8c24b00ad3db31c6662b2
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-15 11:32:55 +0530

    qwertyu

--- main.tf
+++ main.tf
@@ -18,11 +18,15 @@ module "network" {
   source = "./modules/network"
 }
 
-# General EC2 (e.g., bastion or utility instance)
 module "general_ec2" {
-  source    = "./modules/ec2"
-  vpc_id    = module.network.vpc_id
-  subnet_id = module.network.subnet_public_1_id
+  source = "./modules/ec2"
+
+  vpc_id             = module.network.vpc_id
+  subnet_id          = module.network.subnet_public_1_id
+
+  subnet_public_1_id = module.network.subnet_public_1_id
+  subnet_public_2_id = module.network.subnet_public_2_id
+  ecs_cluster_id     = module.ecs_cluster.ecs_cluster_id
 }
 
 # App EC2 (used with ECS etc.)
