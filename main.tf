58853e80e01880fc899bda6ada0b7fd85985cdc9
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-15 11:29:53 +0530

    wqwert

--- main.tf
+++ main.tf
@@ -29,6 +29,9 @@ module "general_ec2" {
 module "app_ec2" {
   source = "./modules/ec2"
 
+  vpc_id             = module.network.vpc_id
+  subnet_id          = module.network.subnet_public_1_id
+
   subnet_public_1_id = module.network.subnet_public_1_id
   subnet_public_2_id = module.network.subnet_public_2_id
   ecs_cluster_id     = module.ecs_cluster.ecs_cluster_id
