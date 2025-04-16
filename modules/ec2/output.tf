9b470c230f1bd03aaeb8791ca61cadded837ce85
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-16 12:18:37 +0530

    qwerty

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -1,9 +1,12 @@
 # modules/ec2/outputs.tf
-output "instance_id" {
-  value = aws_instance.ec2_instance.id
-}
+#output "instance_id" {
+ # value = aws_instance.ec2_instance.id
+#}
 
 output "ecs_instance_sg_id" {
   value = aws_security_group.ecs_instance_sg.id
 }
 
+output "asg_name" {
+  value = aws_autoscaling_group.ecs_asg.name
+}
