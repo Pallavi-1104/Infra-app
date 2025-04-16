25b9d9523085f8b6e54f72794650f4fe85c587d4
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-16 16:47:02 +0530

    qwerty

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -105,6 +105,34 @@ resource "aws_ecs_service" "prometheus_grafana" {
   depends_on = [aws_lb_listener.prometheus_listener]
 }
 
+resource "aws_lb_target_group" "prometheus_tg" {
+  name     = "prometheus-tg"
+  port     = 9090
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "prometheus_listener" {
+  load_balancer_arn = var.alb_arn
+  port              = 9090
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.prometheus_tg.arn
+  }
+}
+
+
 #resource "aws_instance" "ec2_instance" {
   #ami                         = "ami-00a929b66ed6e0de6"
   #instance_type               = "t2.micro"
