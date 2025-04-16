ea4e8dc3d243b78763cfb5316d62ca8ebf8cf21f
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-16 15:21:56 +0530

    qwerty

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -22,3 +22,13 @@ resource "aws_iam_role_policy_attachment" "ec2_attach_policy" {
   role       = aws_iam_role.this.name
   policy_arn = var.policy_arn
 }
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_assume_role_policy.json
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecsTaskRole"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role_policy.json
+}
